e68bbb709705f0ffcc58226a67837e6dff6b217d
tsujio <ntsujio@gmail.com>
2023-05-23 14:22:30 +0900

    Update

--- go.mod
+++ go.mod
@@ -3,9 +3,10 @@ module github.com/tsujio/game-bullet-hell
 go 1.19
 
 require (
-	github.com/hajimehoshi/ebiten/v2 v2.5.3
+	github.com/hajimehoshi/ebiten/v2 v2.5.4
 	github.com/samber/lo v1.38.1
-	github.com/tsujio/game-util v0.0.0-20230428133538-f9a01a3e8095
+	github.com/tsujio/game-util v0.0.0-20230520081749-71d053d62722
+	github.com/tsujio/go-bulletml v0.2.0
 )
 
 require (
@@ -16,11 +17,11 @@ require (
 	github.com/jezek/xgb v1.1.0 // indirect
 	github.com/jfreymuth/oggvorbis v1.0.5 // indirect
 	github.com/jfreymuth/vorbis v1.0.2 // indirect
-	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
-	golang.org/x/exp/shiny v0.0.0-20230425010034-47ecfdc1ba53 // indirect
+	golang.org/x/exp v0.0.0-20230519143937-03e91628a987 // indirect
+	golang.org/x/exp/shiny v0.0.0-20230519143937-03e91628a987 // indirect
 	golang.org/x/image v0.7.0 // indirect
 	golang.org/x/mobile v0.0.0-20230427221453-e8d11dd0ba41 // indirect
-	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/sync v0.2.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"image/color"
 	"log"
-	"math"
 	"math/rand"
 	"os"
 	"strconv"
@@ -15,41 +15,168 @@ import (
 	"github.com/samber/lo"
 	"github.com/tsujio/game-bullet-hell/touchutil"
 	"github.com/tsujio/game-util/mathutil"
+	"github.com/tsujio/go-bulletml"
 )
 
 const (
-	gameName     = "bullet-hell"
-	screenWidth  = 640
-	screenHeight = 480
+	gameName      = "bullet-hell"
+	screenWidth   = 640
+	screenHeight  = 480
+	playerR       = 4
+	playerBulletR = 3
+	enemyR        = 4
+	bulletR       = 3
 )
 
-type Enemy struct {
-	pos *mathutil.Vector2D
-	r   float64
+//go:embed resources/*.xml
+var resources embed.FS
+
+var (
+	playerImg, playerBulletImg, enemyImg, bulletImg *ebiten.Image
+)
+
+func init() {
+	playerImg = ebiten.NewImage(playerR*2, playerR*2)
+	vector.DrawFilledCircle(playerImg, playerR, playerR, playerR, color.Black, true)
+
+	playerBulletImg = ebiten.NewImage(playerBulletR*2, playerBulletR*2)
+	vector.DrawFilledCircle(playerBulletImg, playerBulletR, playerBulletR, playerBulletR, color.Black, true)
+
+	enemyImg = ebiten.NewImage(enemyR*2, enemyR*2)
+	vector.DrawFilledCircle(enemyImg, enemyR, enemyR, enemyR, color.Black, true)
+
+	bulletImg = ebiten.NewImage(bulletR*2, bulletR*2)
+	vector.DrawFilledCircle(bulletImg, bulletR, bulletR, bulletR, color.Black, true)
 }
 
-func (e *Enemy) Draw(dst *ebiten.Image) {
-	vector.DrawFilledRect(dst, float32(e.pos.X-e.r), float32(e.pos.Y-e.r), float32(e.r)*2, float32(e.r)*2, color.Black, true)
+type Enemy struct {
+	pos, prevPos *mathutil.Vector2D
+	r            float64
+	runner       bulletml.Runner
+	game         *Game
+}
+
+func (e *Enemy) update() error {
+	e.prevPos = e.pos.Clone()
+
+	if e.runner != nil {
+		if err := e.runner.Update(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (e *Enemy) draw(dst *ebiten.Image) {
+	opts := &ebiten.DrawImageOptions{}
+	w, h := enemyImg.Size()
+	opts.GeoM.Translate(e.pos.X-float64(w)/2, e.pos.Y-float64(h)/2)
+	dst.DrawImage(enemyImg, opts)
 }
 
 type Bullet struct {
-	pos *mathutil.Vector2D
-	r   float64
-	v   *mathutil.Vector2D
-	hit bool
+	pos, prevPos *mathutil.Vector2D
+	r            float64
+	hit          bool
+	runner       bulletml.BulletRunner
+	game         *Game
 }
 
-func (b *Bullet) Draw(dst *ebiten.Image) {
-	vector.DrawFilledCircle(dst, float32(b.pos.X), float32(b.pos.Y), float32(b.r), color.Black, true)
+func (b *Bullet) update() error {
+	b.prevPos = b.pos.Clone()
+
+	if err := b.runner.Update(); err != nil {
+		return err
+	}
+
+	b.pos.X, b.pos.Y = b.runner.Position()
+
+	return nil
+}
+
+func (b *Bullet) draw(dst *ebiten.Image) {
+	opts := &ebiten.DrawImageOptions{}
+	w, h := bulletImg.Size()
+	opts.GeoM.Translate(b.pos.X-float64(w)/2, b.pos.Y-float64(h)/2)
+	dst.DrawImage(bulletImg, opts)
 }
 
 type Player struct {
-	pos *mathutil.Vector2D
-	r   float64
+	pos, prevPos *mathutil.Vector2D
+	r            float64
+	game         *Game
 }
 
-func (p *Player) Draw(dst *ebiten.Image) {
-	vector.DrawFilledCircle(dst, float32(p.pos.X), float32(p.pos.Y), float32(p.r), color.Black, true)
+func (p *Player) update() error {
+	p.prevPos = p.pos.Clone()
+
+	if len(p.game.touches) > 0 {
+		if p.game.touches[0].IsJustReleased() {
+			p.game.touchPosHistory = make([]*mathutil.Vector2D, 60)
+		} else {
+			curr := p.game.touches[0].Position()
+			p.game.touchPosHistory[p.game.ticks%uint64(len(p.game.touchPosHistory))] = curr
+			if prev := p.game.touchPosHistory[(p.game.ticks-1)%uint64(len(p.game.touchPosHistory))]; prev != nil {
+				diff := curr.Sub(prev)
+				if norm := diff.Norm(); norm > 0 {
+					p.pos = p.pos.Add(diff)
+
+					if p.pos.X < 0 {
+						p.pos.X = 0
+					}
+					if p.pos.X > screenWidth {
+						p.pos.X = screenWidth
+					}
+					if p.pos.Y < 0 {
+						p.pos.Y = 0
+					}
+					if p.pos.Y > screenHeight {
+						p.pos.Y = screenHeight
+					}
+				}
+			}
+		}
+	}
+
+	if p.game.ticks%5 == 0 {
+		b := &PlayerBullet{
+			pos:     p.pos.Clone(),
+			prevPos: p.pos.Clone(),
+			r:       playerBulletR,
+		}
+		p.game.playerBullets = append(p.game.playerBullets, b)
+	}
+
+	return nil
+}
+
+func (p *Player) draw(dst *ebiten.Image) {
+	opts := &ebiten.DrawImageOptions{}
+	w, h := playerImg.Size()
+	opts.GeoM.Translate(p.pos.X-float64(w)/2, p.pos.Y-float64(h)/2)
+	dst.DrawImage(playerImg, opts)
+}
+
+type PlayerBullet struct {
+	pos, prevPos *mathutil.Vector2D
+	r            float64
+	hit          bool
+}
+
+func (b *PlayerBullet) update() error {
+	b.prevPos = b.pos.Clone()
+
+	b.pos = b.pos.Add(mathutil.NewVector2D(0, -10))
+
+	return nil
+}
+
+func (b *PlayerBullet) draw(dst *ebiten.Image) {
+	opts := &ebiten.DrawImageOptions{}
+	w, h := playerBulletImg.Size()
+	opts.GeoM.Translate(b.pos.X-float64(w)/2, b.pos.Y-float64(h)/2)
+	dst.DrawImage(playerBulletImg, opts)
 }
 
 type Touch struct {
@@ -63,8 +190,9 @@ type Game struct {
 	ticks           uint64
 	touchPosHistory []*mathutil.Vector2D
 	player          *Player
+	enemy           *Enemy
 	bullets         []*Bullet
-	enemies         []*Enemy
+	playerBullets   []*PlayerBullet
 }
 
 func (g *Game) Update() error {
@@ -72,57 +200,15 @@ func (g *Game) Update() error {
 
 	g.ticks++
 
-	if len(g.touches) > 0 {
-		if g.touches[0].IsJustReleased() {
-			g.touchPosHistory = make([]*mathutil.Vector2D, 60)
-		} else {
-			curr := g.touches[0].Position()
-			g.touchPosHistory[g.ticks%uint64(len(g.touchPosHistory))] = curr
-			if prev := g.touchPosHistory[(g.ticks-1)%uint64(len(g.touchPosHistory))]; prev != nil {
-				diff := curr.Sub(prev)
-				if norm := diff.Norm(); norm > 0 {
-					g.player.pos = g.player.pos.Add(diff)
-
-					if g.player.pos.X < 0 {
-						g.player.pos.X = 0
-					}
-					if g.player.pos.X > screenWidth {
-						g.player.pos.X = screenWidth
-					}
-					if g.player.pos.Y < 0 {
-						g.player.pos.Y = 0
-					}
-					if g.player.pos.Y > screenHeight {
-						g.player.pos.Y = screenHeight
-					}
-				}
-			}
-		}
-	}
-
-	if g.ticks == 1 {
-		for i := 0; i < 2; i++ {
-			e := &Enemy{
-				pos: mathutil.NewVector2D(float64(screenWidth/2+150.0*(i*2-1)), 50),
-				r:   10,
-			}
-			g.enemies = append(g.enemies, e)
-		}
-	}
-
-	if g.ticks%15 == 0 {
-		for _, e := range g.enemies {
-			b := &Bullet{
-				pos: e.pos.Clone(),
-				r:   3,
-				v:   g.player.pos.Sub(e.pos).Normalize().Mul(3),
-			}
-			g.bullets = append(g.bullets, b)
-		}
+	if g.enemy.runner == nil {
+		g.setBulletML("resources/barrage-1.xml")
 	}
 
 	for _, b := range g.bullets {
-		if math.Pow(b.pos.X-g.player.pos.X, 2)+math.Pow(b.pos.Y-g.player.pos.Y, 2) < math.Pow(b.r+g.player.r, 2) {
+		if mathutil.CapsulesCollide(
+			g.player.pos, g.player.prevPos.Sub(g.player.pos), g.player.r,
+			b.pos, b.prevPos.Sub(b.pos), b.r,
+		) {
 			b.hit = true
 
 			g.player.pos = mathutil.NewVector2D(
@@ -134,14 +220,52 @@ func (g *Game) Update() error {
 		}
 	}
 
-	g.bullets = lo.Map(g.bullets, func(b *Bullet, _ int) *Bullet {
-		b.pos = b.pos.Add(b.v)
-		return b
-	})
+	for _, b := range g.playerBullets {
+		if mathutil.CapsulesCollide(
+			g.enemy.pos, g.enemy.prevPos.Sub(g.enemy.pos), g.enemy.r,
+			b.pos, b.prevPos.Sub(b.pos), b.r,
+		) {
+			b.hit = true
+		}
+	}
 
-	g.bullets = lo.Filter(g.bullets, func(b *Bullet, _ int) bool {
-		return !b.hit && b.pos.Sub(mathutil.NewVector2D(screenWidth/2, screenHeight/2)).Norm() < 500
-	})
+	if err := g.player.update(); err != nil {
+		return err
+	}
+
+	if err := g.enemy.update(); err != nil {
+		return err
+	}
+
+	for i, n := 0, len(g.bullets); i < n; i++ {
+		if err := g.bullets[i].update(); err != nil {
+			return err
+		}
+	}
+
+	for i, n := 0, len(g.playerBullets); i < n; i++ {
+		if err := g.playerBullets[i].update(); err != nil {
+			return err
+		}
+	}
+
+	_bullets := g.bullets[:0]
+	for _, b := range g.bullets {
+		if !b.hit &&
+			!b.runner.Vanished() &&
+			b.pos.Sub(mathutil.NewVector2D(screenWidth/2, screenHeight/2)).Norm() < 500 {
+			_bullets = append(_bullets, b)
+		}
+	}
+	g.bullets = _bullets
+
+	_playerBullets := g.playerBullets[:0]
+	for _, b := range g.playerBullets {
+		if !b.hit && b.pos.Sub(mathutil.NewVector2D(screenWidth/2, screenHeight/2)).Norm() < 500 {
+			_playerBullets = append(_playerBullets, b)
+		}
+	}
+	g.playerBullets = _playerBullets
 
 	g.touches = lo.Filter(g.touches, func(t touchutil.Touch, _ int) bool {
 		return !t.IsJustReleased()
@@ -153,15 +277,17 @@ func (g *Game) Update() error {
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.White)
 
+	g.enemy.draw(screen)
+
+	g.player.draw(screen)
+
 	for _, b := range g.bullets {
-		b.Draw(screen)
+		b.draw(screen)
 	}
 
-	for _, e := range g.enemies {
-		e.Draw(screen)
+	for _, b := range g.playerBullets {
+		b.draw(screen)
 	}
-
-	g.player.Draw(screen)
 
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("%.1f", ebiten.ActualFPS()))
 }
@@ -170,18 +296,76 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return screenWidth, screenHeight
 }
 
+func (g *Game) setBulletML(path string) error {
+	f, err := resources.Open(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	bml, err := bulletml.Load(f)
+	if err != nil {
+		return err
+	}
+
+	opts := &bulletml.NewRunnerOptions{
+		OnBulletFired: func(br bulletml.BulletRunner, fc *bulletml.FireContext) {
+			x, y := br.Position()
+			b := &Bullet{
+				pos:     mathutil.NewVector2D(x, y),
+				prevPos: mathutil.NewVector2D(x, y),
+				r:       bulletR,
+				runner:  br,
+				game:    g,
+			}
+			g.bullets = append(g.bullets, b)
+		},
+		CurrentShootPosition: func() (float64, float64) {
+			return g.enemy.pos.X, g.enemy.pos.Y
+		},
+		CurrentTargetPosition: func() (float64, float64) {
+			return g.player.pos.X, g.player.pos.Y
+		},
+	}
+
+	runner, err := bulletml.NewRunner(bml, opts)
+	if err != nil {
+		return err
+	}
+
+	g.enemy.runner = runner
+
+	return nil
+}
+
 func (g *Game) initialize() {
 	g.touches = nil
 	g.touchPosHistory = make([]*mathutil.Vector2D, 60)
+
+	playerPos := mathutil.NewVector2D(
+		screenWidth/2,
+		screenHeight*4/5,
+	)
 	g.player = &Player{
-		pos: mathutil.NewVector2D(
-			screenWidth/2,
-			screenHeight*2/3,
-		),
-		r: 3.0,
+		pos:     playerPos,
+		prevPos: playerPos,
+		r:       playerR,
+		game:    g,
 	}
+
+	enemyPos := mathutil.NewVector2D(
+		screenWidth/2,
+		screenHeight*1/5,
+	)
+	g.enemy = &Enemy{
+		pos:     enemyPos,
+		prevPos: enemyPos,
+		r:       enemyR,
+		game:    g,
+	}
+
 	g.bullets = nil
-	g.enemies = nil
+	g.playerBullets = nil
 }
 
 func main() {
